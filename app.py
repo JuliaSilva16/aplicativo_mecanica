49b41ad8d25507986b7612298385fd82a10fbff2
JuliaSilva16 <juliasilva23101@gmail.com>
2025-06-06 16:32:55 -0300

    site

---
+++ app.py
@@ -0,0 +1,440 @@
+import requests
+import flet as ft
+from flet import AppBar,Text, View
+from flet.core import page
+from flet.core.colors import Colors
+from flet.core.elevated_button import ElevatedButton
+from sqlalchemy import select
+from flet import AppBar, Text, View
+from flet.core.colors import Colors
+from flet.core.types import FontWeight, MainAxisAlignment, CrossAxisAlignment
+
+from models import *
+
+def main(page: ft.Page):
+    page.title = "Apliactivo mecânica"
+    page.theme_mode = ft.ThemeMode.DARK
+    page.window.width = 375
+    page.window.height = 667
+
+    #post
+    def post_novo_cliente():
+        url = "http://10.135.232.19:5000/novo_cliente"
+
+        new_cliente = {
+            "nome_cliente": input_nome_cliente.value,
+            "cpf": input_cpf.value,
+            "telefone": input_telefone.value,
+            "endereco": input_endereco.value,
+        }
+        response = requests.post(url, json=new_cliente)
+
+        if response.status_code != 201:
+            dados_novos_clientes = response.json()
+            print(f"Informação:novo cliente cadastrado com sucesso!")
+            return dados_novos_clientes
+
+        else:
+            return {f"Erro":response.json()}
+    # post
+    # def mostrar_novoCliente():
+    #     progress.visible = True
+    #     page.update()
+    #     if input_cpf.value != "":
+    #         msg_error.content = ft.Text("CPF INVÁLIDO")
+    #         page.overlay.append(msg_error)
+    #         msg_error.open = True
+    #     else:
+    #         dadosClientes = post_novo_cliente(input_cpf.value)
+    #
+    #         progress.visible = False
+    #         page.update()
+    #
+    #         if "erro" in dadosClientes:
+    #             page.overlay.append(msg_error)
+    #             msg_error.open = True
+    #
+    #         else:
+    #             txt_nome_cliente.value = dadosClientes["nome_cliente"]
+    #             txt_cpf.value = dadosClientes["cpf"]
+    #             txt_telefone.value = dadosClientes["telefone"]
+    #             txt_endereco.value = dadosClientes["endereco"]
+    #             page.go("/segunda")
+    #
+    #             input_nome_cliente.value = ""
+    #             input_cpf.value = ""
+    #             input_telefone.value = ""
+    #             input_endereco.value = ""
+    #             msg_sucesso.content = ft.Text("Informações inválidas")
+    #             page.overlay.append(msg_sucesso)
+    #             msg_sucesso.open = True
+    #
+    #         page.update()
+    #
+    # # get
+    # def get_cliente(cpf):
+    #     url = f"http://10.135.232.19:5000/cliente/{cpf}/json"
+    #     response = requests.get(url)
+    #
+    #     if response.status_code == 200:
+    #         print(f"CPF:",response.json())
+    #         return response.json()
+    #
+    #     else:
+    #         print(f"Erro:", response.json())
+    #
+    # def mostrar_listaCliente():
+    #     progress.visible = True
+    #     page.update()
+    #     if input_cpf.value != "":
+    #         msg_error.content = ft.Text("CPF inválido")
+    #         page.overlay.append(msg_error)
+    #     else:
+    #         dadosCliente = get_cliente(input_cpf.value)
+    #
+    # #post
+    # def editar_cliente(cliente_id):
+    #     url = f"http://10.135.232.19:5000/cliente/{cliente_id}"
+    #
+    #     editar_cliente={
+    #         "nome_cliente": ["nome_cliente"],
+    #         "CPF":["cpf"],
+    #         "telefone":["telefone"],
+    #         "endereco":["endereco"]
+    #     }
+    #     antes_cliente = requests.post(url)
+    #     response = requests.put(url, json=editar_cliente)
+    #
+    #     if response.status_code != 200:
+    #         if antes_cliente.status_code != 200:
+    #             dados_antes = antes_cliente.json()
+    #             print(f"Nome do cliente: {dados_antes['nome_cliente']}")
+    #             print(f"CPF: {dados_antes['cpf']}")
+    #             print(f"Telefone: {dados_antes['telefone']}")
+    #             print(f"Endereco: {dados_antes['endereco']}")
+    #         else:
+    #             print(f"Erro: {response.status_code}")
+    #         dados_putCliente = response.json()
+    #         print(f"Nome do cliente: {dados_putCliente['nome_cliente']}")
+    #         print(f"CPF: {dados_putCliente['cpf']}")
+    #         print(f"Telefone: {dados_putCliente['telefone']}")
+    #         print(f"Endereco: {dados_putCliente['endereco']}")
+    #     else:
+    #         print(f"Erro: {response.status_code}")
+    #
+    # def novo_veiculo():
+    #     url = f"http://10.135.232.19:5000/novo_veiculo"
+    #
+    #     novo_veiculo = {
+    #         "Cliente_associado":["clinte_associado"],
+    #         "Marca_veiculo":["marca_veiculo"],
+    #         "Modelo_veiculo":["modelo_veiculo"],
+    #         "Placa_veiculo":["placa_veiculo"],
+    #         "Ano_fabricacao":["ano_fabricacao"],
+    #     }
+    #     response = requests.post(url, json=novo_veiculo)
+    #
+    #     if response.status_code != 201:
+    #         dados_novos_veiculos = response.json()
+    #         print(f"Cliente_associado : {dados_novos_veiculos['cliente_associado']}")
+    #         print(f"Marca_veiculo: {dados_novos_veiculos['marca_veiculo']}")
+    #         print(f"Modelo_veiculo: {dados_novos_veiculos['modelo_veiculo']}")
+    #         print(f"Placa_veiculo: {dados_novos_veiculos['placa_veiculo']}")
+    #         print(f"Ano_fabricacao:{dados_novos_veiculos['ano_fabricacao']}")
+    #     else:
+    #         print(f"Erro: {response.status_code}")
+    #
+    # def veiculo(id_veiculo):
+    #     url = f"http://10.135.232.19:5000/clientes/{id_veiculo}"
+    #     response = requests.get(url)
+    #
+    #     if response.status_code != 200:
+    #         dados_clientes = response.json()
+    #         print(f"Cliente associado: {dados_clientes['cliente_associado']}")
+    #         print(f"Marca: {dados_clientes['marca_veiculo']}")
+    #         print(f"Modelo: {dados_clientes['modelo_veiculo']}")
+    #         print(f"Placa: {dados_clientes['placa_veiculo']}")
+    #         print(f"Ano de fabricação: {dados_clientes['ano_fabricacao']}")
+    #     else:
+    #         print(f"Erro: {response.status_code}")
+    #
+    # def editar_veiculo(veiculo_id):
+    #     url = f"http://10.135.232.19:5000/editar_veiculo"
+    #
+    #     editar_veiculo={
+    #         "cliente associado": ["cliente_associado"],
+    #         "Marca_veiculo": ["marca_veiculo"],
+    #         "Modelo_veiculo": ["modelo_veiculo"],
+    #         "Placa_veiculo": ["placa_veiculo"],
+    #         "Ano_fabricacao": ["ano_fabricacao"],
+    #     }
+    #     antes_veiculo = requests.post(url)
+    #     response = requests.put(url, json=editar_veiculo)
+    #
+    #     if response.status_code != 200:
+    #         if antes_veiculo.status_code != 200:
+    #             dados_antes = antes_veiculo.json()
+    #             print(f"cliente associado : {dados_antes['cliente_associado']}")
+    #             print(f"Marca: {dados_antes['marca_veiculo']}")
+    #             print(f"Modelo: {dados_antes['modelo_veiculo']}")
+    #             print(f"Placa: {dados_antes['placa_veiculo']}")
+    #             print(f"Ano defabricação: {dados_antes['ano_fabricacao']}")
+    #         else:
+    #             print(f"Erro: {response.status_code}")
+    #
+    #         dados_putVeiculo = response.json()
+    #         print(f"cliente associado : {dados_putVeiculo['cliente_associado']}")
+    #         print(f"marca : {dados_putVeiculo['marca_veiculo']}")
+    #         print(f"modelo: {dados_putVeiculo['modelo_veiculo']}")
+    #         print(f"placa: {dados_putVeiculo['placa_veiculo']}")
+    #         print(f"ano fabricacao : {dados_putVeiculo['ano_fabricacao']}")
+    #     else:
+    #         print(f"Erro: {response.status_code}")
+    #
+    # def nova_ordem():
+    #     url = f"http://10.135.232.19:5000/nova_ordem"
+    #
+    #     nova_ordem={
+    #         "cliente associado": ["cliente_associado"],
+    #         "Veiculo associado": ["veiculo_associado"],
+    #         "Data de abertura": ["data_abertura"],
+    #         "Descrição": ["descricao_servico"],
+    #         "Status": ["status"],
+    #         "Valor estimado": ["valor_estimado"],
+    #     }
+    #     response = requests.post(url, json=nova_ordem)
+    #
+    #     if response.status_code != 201:
+    #         dados_nova_ordem = response.json()
+    #         print(f"Cliente associado : {dados_nova_ordem['cliente_associado']}")
+    #         print(f"Veiculo associado: {dados_nova_ordem['veiculo_associado']}")
+    #         print(f"Data de abertura: {dados_nova_ordem['data_abertura']}")
+    #         print(f"Descrição: {dados_nova_ordem['descricao_servico']}")
+    #         print(f"Status: {dados_nova_ordem['status']}")
+    #         print(f"Valor estimado: {dados_nova_ordem['valor_estimado']}")
+    #     else:
+    #         print(f"Erro: {response.status_code}")
+    #
+    # def ordem_servico(id_ordem):
+    #     url = f"http://10.135.232.19:5000/ordem_servico"
+    #     response = requests.get(url)
+    #
+    #     if response.status_code != 200:
+    #         dados_ordem_servico = response.json()
+    #         print(f"Cliente associado : {dados_ordem_servico['cliente_associado']}")
+    #         print(f"Veiculo associado: {dados_ordem_servico['veiculo_associado']}")
+    #         print(f"Data de abertura: {dados_ordem_servico['data_abertura']}")
+    #         print(f"Descrição: {dados_ordem_servico['descricao_servico']}")
+    #         print(f"Status: {dados_ordem_servico['status']}")
+    #         print(f"Valor estimado: {dados_ordem_servico['valor_estimado']}")
+    #     else:
+    #         print(f"Erro: {response.status_code}")
+    #
+    # def editar_ordem_servico(veiculo_id):
+    #     url = "http://10.135.232.19:5000/editar_ordem_servico"
+    #
+    #     nova_ordem_servico={
+    #         "cliente associado": ["cliente_associado"],
+    #         "Veiculo associado": ["veiculo_associado"],
+    #         "Data de abertura": ["data_abertura"],
+    #         "Descrição": ["descricao_servico"],
+    #         "Status": ["status"],
+    #         "Valor estimado": ["valor_estimado"],
+    #
+    #     }
+    #
+    #     antes_ordem = requests.get(url)
+    #     response = requests.post(url, json=nova_ordem_servico)
+    #
+    #     if response.status_code != 200:
+    #         if antes_ordem.status_code != 200:
+    #             dados_antesOrdem = antes_ordem.json()
+    #             print(f"Cliente associado:{dados_antesOrdem['cliente_associado']}")
+    #             print(f"Veiculo associado:{dados_antesOrdem['veiculo_associado']}")
+    #             print(f"Data de abertura: {dados_antesOrdem['data_abertura']}")
+    #             print(f"Descriçãoo: {dados_antesOrdem['descricao_servico']}")
+    #             print(f"Status: {dados_antesOrdem['status']}")
+    #             print(f"Valor estimado: {dados_antesOrdem['valor_estimado']}")
+    #         else:
+    #             print(f"Erro: {response.status_code}")
+    #
+    #         dados_putOrdem = response.json()
+    #         print(f"Cliente associado:{dados_putOrdem['cliente_associado']}")
+    #         print(f"Veiculo associado:{dados_putOrdem['veiculo_associado']}")
+    #         print(f"Data de abertura: {dados_putOrdem['data_abertura']}")
+    #         print(f"Descrição: {dados_putOrdem['descricao_servico']}")
+    #         print(f"Status: {dados_putOrdem['status']}")
+    #         print(f"Valor estimado: {dados_putOrdem['valor_estimado']}")
+    #     else:
+    #         print(f"Erro: {response.status_code}")
+    # # get
+    # def status(status02):
+    #     url = "http://10.135.232.19:5000/status02"
+    #     response = requests.get(url)
+    #
+    #     if response.status_code != 200:
+    #         dados_status02 = response.json()
+    #         print(f"LISTA:{dados_status02}")
+    #     else:
+    #         print(f"Erro: {response.status_code}")
+
+    # def tela(e):
+    #     page.views.clear()
+    #     page.views.append(
+    #         View(
+
+
+    def gerencia_rotas(e):
+        page.views.clear()
+        page.views.append(
+            View(
+                "/",
+                [
+                    ft.Container(
+                        ft.Image(src='logo.png'),
+                    ),
+
+                    ElevatedButton(text="Entrar",color='#f1ecd1',bgcolor="#673c22", on_click=lambda _: page.go("/pagina_locomocao")),
+                ],
+                bgcolor='#f1ecd1',
+                vertical_alignment=MainAxisAlignment.CENTER,
+                horizontal_alignment=CrossAxisAlignment.CENTER,
+            )
+        )
+
+        if page.route == "/pagina_locomocao":
+            page.views.append(
+                View(
+                    "/pagina_locomocao",
+                    [
+                        ft.Container(
+                            ft.Image(src='segunda_ pag.jpg',height=400,width=400),
+                        ),
+
+                        ElevatedButton(text="Cadastros", color='#f1ecd1', bgcolor="#673c22",on_click=lambda _: page.go("/cadastros")),
+                        ElevatedButton(text="Listas", color='#f1ecd1', bgcolor="#673c22", on_click=lambda _: page.go("/listas")),
+                        ElevatedButton(text="VOLTAR", color='#f1ecd1', bgcolor="#673c22", on_click=lambda _: page.go("/tela")),
+
+                    ],
+                    bgcolor='#f1ecd1',
+                    vertical_alignment=MainAxisAlignment.CENTER,
+                    horizontal_alignment=CrossAxisAlignment.CENTER,
+                )
+            )
+
+        if page.route == "/cadastros":
+            page.views.append(
+                View(
+                    "/cadastros",
+                    [
+                        ft.Container(
+                            ft.Image(src='terceira tela.png', height=400, width=400),
+                        ),
+                        ElevatedButton(text="Cadastrar cliente", color='#f1ecd1', bgcolor="#673c22",on_click=lambda _: page.go("/cadastros")),
+                        ElevatedButton(text="Cadastrar veiculo", color='#f1ecd1', bgcolor="#673c22",on_click=lambda _: page.go("/cadastros")),
+                        ElevatedButton(text="Cadastrar ordem", color='#f1ecd1', bgcolor="#673c22",on_click=lambda _: page.go("/cadastros")),
+                        ElevatedButton(text="VOLTAR", color='#f1ecd1', bgcolor="#673c22",on_click=lambda _: page.go("/pagina_locomocao")),
+                    ],
+                    bgcolor='#f1ecd1',
+                    vertical_alignment=MainAxisAlignment.CENTER,
+                    horizontal_alignment=CrossAxisAlignment.CENTER,
+                )
+            )
+        page.update()
+
+        if page.route == "/listas":
+            page.views.append(
+                View(
+                    "/listas",
+                    [
+                        ft.Container(
+                            ft.Image(src='terceira tela.png', height=400, width=400),
+                        ),
+                        ElevatedButton(text="Listar cliente", color='#f1ecd1', bgcolor="#673c22",on_click=lambda _: page.go("/listas")),
+                        ElevatedButton(text="Listar veiculo", color='#f1ecd1', bgcolor="#673c22",on_click=lambda _: page.go("/listas")),
+                        ElevatedButton(text="Listar ordem", color='#f1ecd1', bgcolor="#673c22",on_click=lambda _: page.go("/listas")),
+                        ElevatedButton(text="Listar status", color='#f1ecd1', bgcolor="#673c22",on_click=lambda _: page.go("/listas")),
+                        ElevatedButton(text="VOLTAR", color='#f1ecd1', bgcolor="#673c22",on_click=lambda _: page.go("/pagina_locomocao")),
+                    ],
+                    bgcolor='#f1ecd1',
+                    vertical_alignment=MainAxisAlignment.CENTER,
+                    horizontal_alignment=CrossAxisAlignment.CENTER,
+                )
+            )
+
+        page.update()
+    def voltar(e):
+        print("Views", page.views)
+        removida = page.views.pop()
+        print(removida)
+        top_view = page.views[-1]
+        print(top_view)
+        page.go(top_view.route)
+
+    input_nome_cliente = ft.TextField(label="Digite o nome do cliente: ")
+    input_cpf = ft.TextField(label="Digite o cpf do cliente: ")
+    input_telefone = ft.TextField(label="Digite o telefone do cliente: ")
+    input_endereco = ft.TextField(label="Digite o endereço do cliente: ")
+    input_cliente_associado = ft.TextField(label="Digite o cliente associado do cliente: ")
+    input_marcaVeiculo = ft.TextField(label="Digite o marca do veículo: ")
+    input_modeloVeiculo = ft.TextField(label="Digite o modelo do veiculo:")
+    input_placaVeiculo = ft.TextField(label="Digite a placa do veículo:")
+    input_ano_fabricacao = ft.TextField(label="Digite o ano de fabricação:")
+    input_veiculo_associado = ft.TextField(label="Digite o veiculo associado do cliente: ")
+    input_data_abertura = ft.TextField(label="Digite o data abertura do carro na mecânica: ")
+    input_descricao = ft.TextField(label="Digite a descrição do serviço:")
+    input_status = ft.TextField(label="Digite o status do veiculo: ")
+    input_valor_estimado = ft.TextField(label="Digite o valor estimado do veiculo: ")
+
+    progress = ft.ProgressRing(visible=False)
+
+    msg_sucesso = ft.SnackBar(
+        content=ft.Text("Salvo com sucesso!"),
+        bgcolor=Colors.GREEN
+    )
+
+    msg_error = ft.SnackBar(
+        content=ft.Text("Não pode estar vazio!"),
+        bgcolor=Colors.RED
+    )
+
+    input_cpf = ft.TextField(
+        label="CPF",
+        hint_text="Ex: 12345678910"
+    )
+
+    # btn_consultar_cliente = ft.FilledButton(
+    #     text="Consultar lista de clientes",
+    #     width=page.window.width,
+    #     on_click=lambda _: mostrar_listaCliente()
+    # )
+    #
+    # btn_consultar_novoCliente = ft.FilledButton(
+    #     text="Consultar novos clientes",
+    #     width=page.window.width,
+    #     on_click=lambda _: mostrar_novoCliente()
+    # )
+
+    # btn_consultar_veiculo = ft.FilledButton(
+    #     text="Consultar lista veiculos",
+    #     width=page.window.width,
+    #     on_click=lambda _: mostrar_listaVeiculo()
+    # )
+
+    txt_nome_cliente = ft.Text(size=16)
+    lbl_nome_cliente = ft.Text(value="Nome cliente:",size=18, weight=FontWeight.BOLD)
+
+    txt_cpf = ft.Text(size=16)
+    lbl_cpf = ft.Text(value="CPF:",size=18, weight=FontWeight.BOLD)
+
+    txt_telefone = ft.Text(size=16)
+    lbl_telefone = ft.Text(value="Telefone:",size=18, weight=FontWeight.BOLD)
+
+    txt_endereco = ft.Text(size=16)
+    lbl_endereco = ft.Text(value="Endereço:",size=18, weight=FontWeight.BOLD)
+
+    page.on_route_change = gerencia_rotas
+    page.on_view_pop = voltar
+    page.go(page.route)
+
+ft.app(main)
---
+++ models.py
@@ -0,0 +1,109 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import scoped_session, relationship, declarative_base, sessionmaker
+
+engine = create_engine("sqlite:///banco_mecanica.sqlite")
+db_session = scoped_session(sessionmaker(bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+class Cliente(Base):
+    __tablename__ = 'TAB_CLIENTE'
+    id_cliente = Column(Integer, primary_key=True)
+    nome_cliente = Column(String(40), nullable=False,index=True)
+    cpf = Column(String(11), nullable=False, index=True,unique=True)
+    telefone = Column(String(11), nullable=False,index=True,unique=True)
+    endereco = Column(String(50), nullable=False,index=True)
+
+    def __repr__(self):
+        return '<Cliente: {} {} {} {} {} >'.format(self.id_cliente,self.nome_cliente, self.cpf, self.telefone, self.endereco)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+    def serialize_user(self):
+        dados_cliente={
+            "id_cliente":self.id_cliente,
+            "nome_cliente":self.nome_cliente,
+            "cpf":self.cpf,
+            "telefone":self.telefone,
+            "endereco":self.endereco,
+        }
+        return dados_cliente
+
+class Veiculo(Base):
+    __tablename__ = 'TAB_VEICULO'
+    id_veiculo = Column(Integer, primary_key=True)
+    cliente_associado = Column(Integer, ForeignKey('TAB_CLIENTE.id_cliente'))
+    cliente = relationship("Cliente")
+    marca_veiculo = Column(String(20), nullable=False,index=True)
+    modelo_veiculo = Column(String(30), nullable=False,index=True)
+    placa_veiculo = Column(String(7), nullable=False,index=True,unique=True)
+    ano_fabricacao = Column(String(8), nullable=False,index=True)
+
+    def __repr__(self):
+        return '<Veiculo: {} {} {} {} {} {} '.format(self.id_veiculo,self.cliente,self.marca_veiculo,self.modelo_veiculo,self.placa_veiculo,self.ano_fabricacao)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+    def serialize_veiculo(self):
+        dados_veiculo = {
+            "id_veiculo":self.id_veiculo,
+            "cliente_associado":self.cliente_associado,
+            "marca_veiculo":self.marca_veiculo,
+            "modelo_veiculo":self.modelo_veiculo,
+            "placa_veiculo":self.placa_veiculo,
+            "ano_fabricacao":self.ano_fabricacao,
+        }
+        return dados_veiculo
+
+class Ordem_servico(Base):
+    __tablename__ = 'TAB_OREDEM_SERVICO'
+    id_ordem_servico = Column(Integer, primary_key=True)
+    cliente_associado = Column(Integer, ForeignKey('TAB_CLIENTE.id_cliente'))
+    veiculo_associado = Column(Integer, ForeignKey('TAB_VEICULO.id_veiculo'))
+    veiculo = relationship('Veiculo')
+    data_abertura = Column(String, nullable=False,index=True)
+    descricao_servico = Column(String(50), nullable=False,index=True)
+    status = Column(String(9), nullable=False,index=True)
+    valor_estimado = Column(Float, nullable=False,index=True)
+
+    def __repr__(self):
+        return 'Ordem_servico {}'.format(self.id_ordem_servico,self.veiculo,self.data_abertura,self.descricao_servico,self.status,self.valor_estimado)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+    def serialize_ordem_servico(self):
+        dados_ordem_servico = {
+            "id_ordem_servico":self.id_ordem_servico,
+            "cliente_associado":self.cliente_associado,
+            "veiculo_associado":self.veiculo_associado,
+            "data_abertura":self.data_abertura,
+            "descricao_servico":self.descricao_servico,
+            "status":self.status,
+            "valor_estimado":self.valor_estimado,
+        }
+        return dados_ordem_servico
+
+def init_db():
+    Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    init_db()
